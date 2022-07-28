95ef155f34bc1c66d9d874a1a8c38a1de2dde44b
anushagh <89003643+anushagh@users.noreply.github.com>
2022-07-28 16:03:32 +0530

    Update test.sv

--- test.sv
+++ test.sv
@@ -3,35 +3,33 @@
 
 class transactor;
 
-rand bit serial_in;
-rand bit [3:0] parallel_in;
-rand bit dir,load;
+rand bit sl_in;
+rand bit [3:0] pl_in;
+rand bit d,load;
 
 endclass
 
 class driver;
-
-transactor trx;
+transactor tx;
 virtual inter.TB T1;
 
 function new(virtual inter.TB T2);
 begin
 	this.T1=T2;
-	this.trx=new();
+	this.tx=new();
 end
 endfunction
 
-task drive_data();
+task drive();
 begin
 	repeat(100) @(T1.cb)
 	begin
-		if((trx.randomize()))
+		if((tx.randomize()))
 		begin
-			T1.cb.serial_in <= trx.serial_in;
-			T1.cb.dir<= trx.dir;
-			T1.cb.parallel_in<=trx.parallel_in;
-			T1.cb.load<=trx.load;
-			//$display("%d : driver: sent A=%b, B=%b and opcode=%b",$time, i1.cb.a,i1.cb.b,i1.cb.opcode);
+			T1.cb.s_in <= tx.sl_in;
+			T1.cb.d<= tx.d;
+			T1.cb.p_in<=tx.pl_in;
+			T1.cb.load<=tx.load;
 		end
 		else
 		$display("error encountered");
@@ -46,9 +44,10 @@ program test(inter.TB T3);
 
 initial
 begin
-	T3.reset <= 0;
-	#10 T3.reset <=1;
-	#10 T3.reset <=0;
+	T3.reset <= 1;
+	#10 T3.reset <= 0;
+	#100 T3.reset <=1;
+	#100 T3.reset <=0;
 	#1000 $finish;
 end
 
@@ -56,7 +55,7 @@ initial
 begin
 	driver drv;
 	drv=new(T3);
-	drv.drive_data();
+	drv.drive();
 end
 
 endprogram
